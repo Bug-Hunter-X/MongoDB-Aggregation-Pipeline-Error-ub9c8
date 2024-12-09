6a8de244174f7465aeeab0e3a05759276275935c
BugHunter-X <askzenai@gmail.com>
2024-12-09 18:38:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,23 @@
+```javascript
+//Corrected aggregation pipeline
+db.collection.aggregate([
+  {
+    $match: { /* some filter */ }
+  },
+  {
+    $group: {
+      _id: "$fieldName",
+      count: { $sum: 1 }
+    }
+  },
+  {
+    $sort: { count: -1 }
+  },
+  {
+    $limit: 10
+  }
+]).toArray(function(err, result){
+  if(err) throw err;
+  console.log(result);
+});
+```
